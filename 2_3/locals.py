984b3a472e4d518c22f71e9ef49377ef22f77353
David Sang-hyun Park <hyun06000@gmail.com>
2021-04-03 00:11:59 +0900

    second day 2

--- 2_3/locals.py
+++ 2_3/locals.py
@@ -4,29 +4,34 @@ import numpy as np
 import pprint
 
 
-class MyClass():
+# case of a function
+
+def func():
+    local_val = np.array([1, 2, 3])
+    pprint.pprint(locals())
+
+func()
+
+# case of a class
+
+class MyClass(object):
     def __init__(self):
         self.a = 100
         self.b = 'hi'
         self.c = True
-        pprint.pprint(locals())
+        pprint.pprint(self.__dict__)
 
     def change_c(self):
         if self.c:
             self.c = False
         else:
             self.c = True
-        pprint.pprint(locals())
+        pprint.pprint(self.__dict__)
 
-
-def func():
-    local_val = np.array([1, 2, 3])
-    pprint.pprint(locals())
 
 a = MyClass()
 
 a.change_c()
 
-func()
-
+pprint.pprint(globals())
 pprint.pprint(locals())
