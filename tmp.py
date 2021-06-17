00a79b1e13017e1510e8702d91a943b98e1c5ace
Sang-hyun Park <hyun06000@gmail.com>
2021-06-17 17:09:34 +0900

    git config --global core.quotepath false

--- tmp.py
+++ tmp.py
@@ -1,11 +1,1 @@
-def rearrange(s):
-    for i in range(-1, -len(s) - 1, -1):
-        print(i)
-        if s[i] == '0':
-            print(s[:len(s)+(i+1)])
-            return s[:len(s)+(i+1)] + '110' + s[len(s)+(i+1):]
-    return '110' + s
-
-
-
-print(rearrange('0111'))
+tmp
