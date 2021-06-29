199e5b167d11777c0280db6dbe89f3d941461591
David Sang-hyun Park <hyun06000@gmail.com>
2021-06-29 21:26:41 +0900

    tictoc

--- tmp.py
+++ tmp.py
@@ -1,5 +1,23 @@
-budget = 32
-hamberger = [3, 7]
-topping = [2, 4]
+from collections import deque
 
-for i in hamberger:
+R, C = map(int, input().split(" "))
+
+board =[]
+for _ in range(R):
+    board.append(input())
+
+di_dj = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+max_len = 0
+queue = deque([(0, 0, board[0][0])])
+while queue:
+    i, j, visited = queue.popleft()
+    max_len = max(max_len, len(visited) )
+    for di, dj in di_dj:
+        _i, _j = i + di, j + dj
+        if 0 <= _i < R and \
+            0 <= _j < C and \
+            not board[_i][_j] in visited:
+            queue.append((_i, _j, visited + board[_i][_j]))
+
+print(max_len)
